2ea7311b3585ef803f1ba5a12b9a72c7f50f58ff
sinan093 <sinankirbas93@web.de>
2024-06-12 10:25:09 +0200

    removed redundat package from project

--- src/main/java/timetracker/customer/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package timetracker.customer.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import timetracker.customer.entity.CustomerEntity;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
-}
